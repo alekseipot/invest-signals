96c8927fff0e1757051e2fadbfce43bcc99781ab
alekseipotapov <aleksei.pot@gmail.com>
2024-10-10 17:01:59 +0200

    add footer

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import AppNavbar from './AppNavbar';
+import Footer from './Footer';
 import { Container } from 'react-bootstrap';
 
 const Layout = () => {
     return (
-        <div>
+        <div className="d-flex flex-column min-vh-100">
             {/* Navbar will be present on all pages */}
             <AppNavbar />
 
-            {/* Container for the page content */}
-            <Container className="mt-5">
-                {/* Outlet is where the individual page content will be rendered */}
+            {/* flex-grow-1 is added to ensure the content area expands */}
+            <Container className="flex-grow-1 mt-5">
                 <Outlet />
             </Container>
+
+            {/* Footer will be present on all pages */}
+            <Footer />
         </div>
     );
 };
