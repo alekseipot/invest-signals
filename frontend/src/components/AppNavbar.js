ba38d460f5fb5a91a41f011f30fc818da8d759de
alekseipotapov <aleksei.pot@gmail.com>
2024-11-07 14:26:12 +0100

    add link to menu

--- frontend/src/components/AppNavbar.js
+++ frontend/src/components/AppNavbar.js
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 const AppNavbar = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Check if a JWT token exists in localStorage to determine if the user is logged in
+        const token = localStorage.getItem("jwtToken");
+        setIsAuthenticated(!!token);
+    }, []);
+
+    const handleLogout = () => {
+        // Remove the token from localStorage and update authentication state
+        localStorage.removeItem("jwtToken");
+        setIsAuthenticated(false);
+        navigate("/"); // Redirect to home after logout
+    };
 
     return (
         <Navbar color="dark" dark expand="md">
@@ -12,10 +27,24 @@ const AppNavbar = () => {
             <NavbarBrand tag={Link} to="/search">Search</NavbarBrand>
             <NavbarToggler onClick={() => { setIsOpen(!isOpen) }}/>
             <Collapse isOpen={isOpen} navbar>
-                <Nav className="justify-content-end" style={{width: "100%"}} navbar>
+                <Nav className="justify-content-end" style={{ width: "100%" }} navbar>
                     <NavItem>
                         <NavLink href="https://github.com/alekseipot/invest-signals">GitHub</NavLink>
                     </NavItem>
+                    {isAuthenticated ? (
+                        <>
+                            <NavItem>
+                                <NavLink tag={Link} to="/dashboard">My Dashboard</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#" onClick={handleLogout}>Logout</NavLink>
+                            </NavItem>
+                        </>
+                    ) : (
+                        <NavItem>
+                            <NavLink tag={Link} to="/login">Login</NavLink>
+                        </NavItem>
+                    )}
                 </Nav>
             </Collapse>
         </Navbar>
